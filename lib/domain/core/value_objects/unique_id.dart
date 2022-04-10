764b818ee0dae27c3579cbd5255eade8e1eb6961
Shahil <shahil1.jha@gmail.com>
2022-04-10 22:23:16 +0545

    style: add comment

--- lib/domain/core/value_objects/unique_id.dart
+++ lib/domain/core/value_objects/unique_id.dart
@@ -9,12 +9,16 @@ class UniqueId extends ValueObject<String> {
 
   const UniqueId._(this.value);
 
+  ///create a UniqueId object with a unique id
   factory UniqueId() {
     return UniqueId._(
       right(const Uuid().v1()),
     );
   }
 
+  ///create a UniqueId object from a given string
+  ///with the assumption that the input value is correct
+  ///and validated from the source from which it is obtained from
   factory UniqueId.fromUniqueString(String uniqueId) {
     return UniqueId._(
       right(uniqueId),
