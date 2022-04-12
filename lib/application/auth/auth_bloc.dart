bc57919791b1610307f8a58b2f857344d46cb600
Shahil <shahil1.jha@gmail.com>
2022-04-12 21:51:56 +0545

    feat: add bloc implementation

--- lib/application/auth/auth_bloc.dart
+++ lib/application/auth/auth_bloc.dart
@@ -1,15 +1,37 @@
 import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:injectable/injectable.dart';
+import 'package:skefra_task_diary/domain/auth/i_auth_facade.dart';
+
+import '../../domain/core/entities/user.dart';
 
 part 'auth_event.dart';
 part 'auth_state.dart';
 part 'auth_bloc.freezed.dart';
 
+@injectable
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
-  AuthBloc() : super(const AuthState.initial()) {
-    on<AuthEvent>((event, emit) {
-      // TODO: implement event handler
-      // event.map(authCheckRequested: authCheckRequested, signedOut: signedOut);
-    });
+  final IAuthFacade _authFacade;
+
+  AuthBloc(this._authFacade) : super(const AuthState.initial()) {
+    on<AuthCheckRequested>(
+      (event, emit) async {
+        final Option<User> userOption = await _authFacade.getSignedInUser();
+        userOption.fold(
+          //none()
+          () => emit(const AuthState.unauthenticated()),
+          //some()
+          (_) => emit(const AuthState.authenticated()),
+        );
+      },
+    );
+
+    on<SignedOut>(
+      (event, emit) async {
+        await _authFacade.signOutUser();
+        emit(const AuthState.unauthenticated());
+      },
+    );
   }
 }
