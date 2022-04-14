440ea27a0781abb0dd157dff6730abad23f02998
Shahil <shahil1.jha@gmail.com>
2022-04-14 23:32:28 +0545

    feat: add interface and note failure

---
+++ lib/domain/notes/i_note_repository.dart
@@ -0,0 +1,16 @@
+import 'package:dartz/dartz.dart';
+import 'package:kt_dart/collection.dart';
+import 'package:skefra_task_diary/domain/notes/entity/note.dart';
+import 'package:skefra_task_diary/domain/notes/note_failures.dart';
+
+abstract class INoteRepository {
+  Stream<Either<NoteFailure, KtList<Note>>> watchAll();
+
+  Stream<Either<NoteFailure, KtList<Note>>> watchUncompleted();
+
+  Future<Either<NoteFailure, Unit>> create(Note note);
+
+  Future<Either<NoteFailure, Unit>> update(Note note);
+
+  Future<Either<NoteFailure, Unit>> delete(Note note);
+}
---
+++ lib/domain/notes/note_failures.dart
@@ -0,0 +1,7 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+part 'note_failures.freezed.dart';
+
+@freezed
+class NoteFailure with _$NoteFailure {
+  const factory NoteFailure.unexpected() = _Unexpected;
+}
