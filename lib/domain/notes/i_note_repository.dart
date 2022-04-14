fd4bbfd94b345d1dfeb149ce155e1e94ba239047
Shahil <shahil1.jha@gmail.com>
2022-04-14 23:33:26 +0545

    style: remove spacing

--- lib/domain/notes/i_note_repository.dart
+++ lib/domain/notes/i_note_repository.dart
@@ -5,12 +5,8 @@ import 'package:skefra_task_diary/domain/notes/note_failures.dart';
 
 abstract class INoteRepository {
   Stream<Either<NoteFailure, KtList<Note>>> watchAll();
-
   Stream<Either<NoteFailure, KtList<Note>>> watchUncompleted();
-
   Future<Either<NoteFailure, Unit>> create(Note note);
-
   Future<Either<NoteFailure, Unit>> update(Note note);
-
   Future<Either<NoteFailure, Unit>> delete(Note note);
 }
