6bb690eb4c9832693fc877f6faabbb836569d5a7
Shahil <shahil1.jha@gmail.com>
2022-04-20 23:05:44 +0545

    feat: implement note actor bloc

--- lib/application/notes/note_actor/note_actor_bloc.dart
+++ lib/application/notes/note_actor/note_actor_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:skefra_task_diary/domain/notes/i_note_repository.dart';
 import 'package:skefra_task_diary/domain/notes/note_failures.dart';
 
 import '../../../domain/notes/entity/note.dart';
@@ -9,9 +10,19 @@ part 'note_actor_state.dart';
 part 'note_actor_bloc.freezed.dart';
 
 class NoteActorBloc extends Bloc<NoteActorEvent, NoteActorState> {
-  NoteActorBloc() : super(const _Initial()) {
+  final INoteRepository _noteRepository;
+  NoteActorBloc(this._noteRepository) : super(const _Initial()) {
     on<_Deleted>(
-      (event, emit) {},
+      (event, emit) async {
+        emit(const NoteActorState.actionInProgress());
+        final possibleFailure = await _noteRepository.delete(event.note);
+        possibleFailure.fold(
+          //left failure
+          (failure) => emit(NoteActorState.deleteFailure(failure)),
+          //right Unit -> success
+          (_) => emit(NoteActorState.deleteSuccess()),
+        );
+      },
     );
   }
 }
