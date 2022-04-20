5a66b5d515fb45797295a64a6239cd3204fb9249
Shahil <shahil1.jha@gmail.com>
2022-04-20 22:51:26 +0545

    feat: implement mutli-stream note watcher bloc

--- lib/application/notes/note_watcher/note_watcher_bloc.dart
+++ lib/application/notes/note_watcher/note_watcher_bloc.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:injectable/injectable.dart';
 import 'package:kt_dart/kt.dart';
@@ -12,6 +15,7 @@ part 'note_watcher_state.dart';
 part 'note_watcher_bloc.freezed.dart';
 
 /*
+  ##FOR SINGLE STREAM 
   If we don't want to change between the completed list and the uncompleted list,
   and only had one event, for example: the "_WatchAllStarted" event only,
   then the following code will suffice to handle a _stream_ :
@@ -26,26 +30,81 @@ part 'note_watcher_bloc.freezed.dart';
   output other data and subsequantly not allow us to emit new states for that event
 */
 
+/*
+  ##FOR WHEN THERE ARE TWO OR MORE STREAMS
+
+  Pass the listiened data from the stream to the event _"_NotesReceived"_ and then let
+  the _"_NotesReceived"_ event bloc execute the operation of emitting the states.
+
+  Since we can cancel the _listein_ of _StreamSubscription_ from one stream and start to
+  _listein_ another, this allows us to switch from two or more different stream of data.
+
+  To do this switching we need store the _StreamSubscription_ to a field,
+  in this case here, we store it into the field:
+    ```
+      StreamSubscription<Either<NoteFailure, KtList<Note>>>? _notesStreamSubcription;
+    ```
+  
+*/
+
 @injectable
 class NoteWatcherBloc extends Bloc<NoteWatcherEvent, NoteWatcherState> {
   final INoteRepository _noteRepository;
+
+  StreamSubscription<Either<NoteFailure, KtList<Note>>>?
+      _notesStreamSubcription;
+
   NoteWatcherBloc(this._noteRepository) : super(const _Initial()) {
     on<_WatchAllStarted>(
-      (event, emit) {
+      (event, emit) async {
         emit(const NoteWatcherState.loadInProgress());
-        _noteRepository.watchAll().map(
-              (failureOrNotes) => failureOrNotes.fold(
-                (failure) => emit(NoteWatcherState.loadFailure(failure)),
-                (notes) => emit(NoteWatcherState.loadSuccess(notes)),
+        //cancel any stream subscription
+        await _notesStreamSubcription?.cancel();
+        _notesStreamSubcription = _noteRepository.watchAll().listen(
+              (failureOrNotes) => add(
+                NoteWatcherEvent.notesReceived(failureOrNotes),
               ),
             );
       },
     );
 
     on<_WatchUncompletedStarted>(
-      (event, emit) {},
+      (event, emit) async {
+        emit(const NoteWatcherState.loadInProgress());
+        //cancel any stream subscription
+        await _notesStreamSubcription?.cancel();
+        // if (_notesStreamSubcription != null) {
+        //   await _notesStreamSubcription!
+        //       .cancel(); //cancel any stream subscription
+        // }
+        _notesStreamSubcription = _noteRepository.watchUncompleted().listen(
+              (failureOrNotes) => add(
+                NoteWatcherEvent.notesReceived(failureOrNotes),
+              ),
+            );
+      },
     );
 
-    // on<>((event, emit) {},);
+    on<_NotesReceived>(
+      (event, emit) {
+        event.failureOrNotes.fold(
+          (failure) => emit(NoteWatcherState.loadFailure(failure)),
+          (notes) => emit(NoteWatcherState.loadSuccess(notes)),
+        );
+      },
+    );
+  }
+
+  @override
+  Future<void> close() async {
+    /*
+      Cancel/Close any stream subscription when exiting the bloc to prevent 
+      memory leak and building of things we don't even see.
+
+      As BLoc itself is a Stream we can override the close() method of the bloc 
+      to close our _StreamSubscription_.
+    */
+    await _notesStreamSubcription?.cancel();
+    return super.close();
   }
 }
--- lib/application/notes/note_watcher/note_watcher_event.dart
+++ lib/application/notes/note_watcher/note_watcher_event.dart
@@ -1,8 +1,17 @@
 part of 'note_watcher_bloc.dart';
 
+/*
+  _"_NotesReceived"_ is used for switching between _watchAllStarted_ and 
+  _watchUncompletedStarted_ event, where both the events require to change events 
+  and the stream they are subscribing to with-in the same bloc.
+
+  _"_NotesReceived"_ will be used from with-in the bloc
+*/
 @freezed
 class NoteWatcherEvent with _$NoteWatcherEvent {
   const factory NoteWatcherEvent.watchAllStarted() = _WatchAllStarted;
   const factory NoteWatcherEvent.watchUncompletedStarted() =
       _WatchUncompletedStarted;
+  const factory NoteWatcherEvent.notesReceived(
+      Either<NoteFailure, KtList<Note>> failureOrNotes) = _NotesReceived;
 }
