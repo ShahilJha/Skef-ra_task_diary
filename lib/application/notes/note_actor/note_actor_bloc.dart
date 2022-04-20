6d7145656b8d75799e1dc018126d14d022993bd5
Shahil <shahil1.jha@gmail.com>
2022-04-20 22:57:37 +0545

    feat: add state n event to note actor bloc

--- lib/application/notes/note_actor/note_actor_bloc.dart
+++ lib/application/notes/note_actor/note_actor_bloc.dart
@@ -1,5 +1,8 @@
 import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:skefra_task_diary/domain/notes/note_failures.dart';
+
+import '../../../domain/notes/entity/note.dart';
 
 part 'note_actor_event.dart';
 part 'note_actor_state.dart';
@@ -7,8 +10,8 @@ part 'note_actor_bloc.freezed.dart';
 
 class NoteActorBloc extends Bloc<NoteActorEvent, NoteActorState> {
   NoteActorBloc() : super(const _Initial()) {
-    on<NoteActorEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+    on<_Deleted>(
+      (event, emit) {},
+    );
   }
 }
