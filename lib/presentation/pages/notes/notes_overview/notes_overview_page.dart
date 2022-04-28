d5cdf6b154d3d8eb460b1046088ce94ec75ad1d9
Shahil <shahil1.jha@gmail.com>
2022-04-28 20:44:13 +0545

    refactor: add const

--- lib/presentation/pages/notes/notes_overview/notes_overview_page.dart
+++ lib/presentation/pages/notes/notes_overview/notes_overview_page.dart
@@ -62,7 +62,7 @@ class NotesOverviewPage extends StatelessWidget {
                 context.read<AuthBloc>().add(const AuthEvent.signedOut());
               },
             ),
-            actions: [
+            actions: const [
               UncompletedSwitch(),
             ],
           ),
