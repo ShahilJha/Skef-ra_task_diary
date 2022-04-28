5d740326cc24318dc109e0bdc23cd01a7022d979
Shahil <shahil1.jha@gmail.com>
2022-04-28 20:43:17 +0545

    feat: implement switch of uncompleted notes

--- lib/presentation/pages/notes/notes_overview/local_widgets/uncompleted_switch.dart
+++ lib/presentation/pages/notes/notes_overview/local_widgets/uncompleted_switch.dart
@@ -1,21 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:skefra_task_diary/application/notes/note_watcher/note_watcher_bloc.dart';
 
-class UncompletedSwitch extends StatelessWidget {
+class UncompletedSwitch extends HookWidget {
   const UncompletedSwitch({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final toggleState = useState(false);
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
       child: InkResponse(
-        onTap: () {},
-        child: const Icon(Icons.check_box_outline_blank),
+        onTap: () {
+          toggleState.value = !toggleState.value;
+          context.read<NoteWatcherBloc>().add(
+                toggleState.value
+                    ? const NoteWatcherEvent.watchUncompletedStarted()
+                    : const NoteWatcherEvent.watchAllStarted(),
+              );
+        },
+        child: AnimatedSwitcher(
+          duration: const Duration(microseconds: 100),
+          transitionBuilder: (child, animation) => ScaleTransition(
+            scale: animation,
+            child: child,
+          ),
+          //TODO: fix: animation not working
+          child: toggleState.value
+              ? const Icon(
+                  Icons.check_box,
+                  key: Key('check_box'),
+                )
+              : const Icon(
+                  Icons.check_box_outline_blank,
+                  key: Key('check_box_ouline_blank'),
+                ),
+        ),
       ),
     );
   }
 }
-
- // IconButton(
-    //   icon: const Icon(Icons.check_box),
-    //   onPressed: () {},
-    // ),
